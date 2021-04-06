b825caa3cbcbe091080394a6bd1dc008378ed899
Oreberry <aubreymonk1@gmail.com>
2021-04-06 19:07:50 +0100

    Fix ESLint problems
    refactor: fix ESLint style problems

--- SmartHomeAppAPI/app/models/user.model.js
+++ SmartHomeAppAPI/app/models/user.model.js
@@ -1,25 +1,26 @@
 const sql = require('./db.js');
 
 // Constructor
-const User = function (user) {
-  this.user_firstname = user.user_firstname;
-  this.user_lastname = user.user_lastname;
-  this.user_email = user.user_email;
-  this.user_password = user.user_password;
-  this.user_salt = user.user_salt;
-};
+class User {
+  constructor(user) {
+    this.user_firstname = user.user_firstname;
+    this.user_lastname = user.user_lastname;
+    this.user_email = user.user_email;
+    this.user_password = user.user_password;
+    this.user_salt = user.user_salt;
+  }
+  static create(newUser, result) {
+    sql.query('INSERT INTO smarthomeapp_users SET ?', newUser, (err, res) => {
+      if (err) {
+        console.log('Error: ', err);
+        result(err, null);
+        return;
+      }
 
-User.create = (newUser, result) => {
-  sql.query('INSERT INTO smarthomeapp_users SET ?', newUser, (err, res) => {
-    if (err) {
-      console.log('Error: ', err);
-      result(err, null);
-      return;
-    }
-
-    console.log('Created user: ', { id: res.insertId, ...newUser });
-    result(null, { id: res.insertId, ...newUser });
-  });
-};
+      console.log('Created user: ', { id: res.insertId, ...newUser });
+      result(null, { id: res.insertId, ...newUser });
+    });
+  }
+}
 
 module.exports = User;
--- SmartHomeAppAPI/app/routes/user.routes.js
+++ SmartHomeAppAPI/app/routes/user.routes.js
@@ -1,6 +1,6 @@
-module.exports = (app) => {
-  const users = require('../controllers/user.controller.js');
+const users = require('../controllers/user.controller.js');
 
+module.exports = (app) => {
   // Create new User
   app.post('/user', users.create);
 };
