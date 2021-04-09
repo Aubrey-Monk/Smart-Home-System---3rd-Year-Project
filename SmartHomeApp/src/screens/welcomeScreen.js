a0c04e90e75d5c8b6243ecdac9d9ecf94fee55f1
Oreberry <aubreymonk1@gmail.com>
2021-04-09 03:39:07 +0100

    Add style to welcome buttons
    style: add style to welcome buttons

--- SmartHomeApp/src/screens/welcomeScreen.js
+++ SmartHomeApp/src/screens/welcomeScreen.js
@@ -2,21 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {View} from 'react-native';
 import {Text, Button} from 'react-native-paper';
+import globalStyle from '../styles/globalStyle';
 
 const WelcomeScreen = (props) => (
-  <View>
-    <Button
-      role="button"
-      mode="contained"
-      onPress={() => props.navigation.navigate('Login')}>
-      <Text>Login</Text>
-    </Button>
-    <Button
-      role="button"
-      mode="contained"
-      onPress={() => props.navigation.navigate('Signup')}>
-      <Text>Signup</Text>
-    </Button>
+  <View style={globalStyle.flexContainer}>
+    <View style={globalStyle.twoButtonView}>
+      <Button
+        role="button"
+        contentStyle={globalStyle.buttonContent}
+        style={globalStyle.button}
+        mode="contained"
+        onPress={() => props.navigation.navigate('Login')}>
+        <Text>Login</Text>
+      </Button>
+
+      <Button
+        role="button"
+        contentStyle={globalStyle.buttonContent}
+        style={globalStyle.button}
+        mode="contained"
+        onPress={() => props.navigation.navigate('Signup')}>
+        <Text>Signup</Text>
+      </Button>
+    </View>
   </View>
 );
 
---
+++ SmartHomeApp/src/styles/globalStyle.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  flexContainer: {
+    flex: 1,
+  },
+  twoButtonView: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  button: {
+    borderRadius: 20,
+    height: '20%',
+    margin: '7%',
+  },
+  buttonContent: {
+    borderRadius: 20,
+    height: '100%',
+  },
+});
