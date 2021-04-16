ec5e8040f490265e483200465abb3da1ba3b919e
Oreberry <aubreymonk1@gmail.com>
2021-04-16 13:52:11 +0100

    Add login implementation to app
    feat: add login implementation to app

---
+++ SmartHomeApp/src/components/login.js
@@ -0,0 +1,38 @@
+import {ToastAndroid} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Login = (props, details) =>
+  fetch('http://10.0.2.2:3333/user/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: details.email,
+      password: details.password,
+    }),
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      if (response.status === 400) {
+        throw new Error('Invalid email or password.');
+      }
+      if (response.status === 500) {
+        throw new Error('Server Error.');
+      } else {
+        throw new Error('Something went wrong.');
+      }
+    })
+    .then(async (responseJson) => {
+      await AsyncStorage.setItem('@session_token', responseJson.token);
+      await AsyncStorage.setItem('@user_id', JSON.stringify(responseJson.id));
+
+      props.navigation.navigate('Welcome');
+    })
+    .catch((error) => {
+      ToastAndroid.show(error.toString(), ToastAndroid.SHORT);
+    });
+
+export default Login;
--- SmartHomeApp/src/screens/loginScreen.js
+++ SmartHomeApp/src/screens/loginScreen.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
-import {Text} from 'react-native-paper';
+import {Button, Text, TextInput} from 'react-native-paper';
+import Login from '../components/login';
 
-const LoginScreen = () => (
-  <View>
-    <Text>Login</Text>
-  </View>
-);
+const LoginScreen = (props) => {
+  // hard coded login details for quick testing
+  const [email, setEmail] = useState('aubrey.monk@mail.com');
+  const [password, setPassword] = useState('password123');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+
+  const submit = () => {
+    const details = {
+      email,
+      password,
+    };
+    Login(props, details);
+  };
+
+  return (
+    <View>
+      <View>
+        <TextInput
+          role="textbox"
+          type="outlined"
+          label="Email"
+          placeholder="Enter email"
+          onChangeText={(value) => setEmail(value)}
+          value={email}
+        />
+        <TextInput
+          role="textbox"
+          secureTextEntry
+          type="outlined"
+          label="Password"
+          placeholder="Enter password"
+          onChangeText={(value) => setPassword(value)}
+          value={password}
+        />
+      </View>
+      <View>
+        <Button role="button" mode="contained" onPress={() => submit()}>
+          <Text>Login</Text>
+        </Button>
+      </View>
+    </View>
+  );
+};
 
 export default LoginScreen;
