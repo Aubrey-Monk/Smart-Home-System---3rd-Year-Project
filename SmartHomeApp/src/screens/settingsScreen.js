d39b8a504237e3fba25f02214da2cb1bff08b29a
Oreberry <aubreymonk1@gmail.com>
2021-05-12 15:50:22 +0100

    Add dark theme toggle
    feat: dark theme toggle

--- SmartHomeApp/src/screens/settingsScreen.js
+++ SmartHomeApp/src/screens/settingsScreen.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {TouchableRipple, Switch, useTheme} from 'react-native-paper';
+import {Context} from '../components/context';
 
-const SettingsScreen = () => (
-  <View>
-    <Text>Hello</Text>
-  </View>
-);
+const SettingsScreen = () => {
+  const paperTheme = useTheme();
+  const {toggleTheme} = React.useContext(Context);
+  return (
+    <View>
+      <TouchableRipple
+        role="button"
+        onPress={() => {
+          toggleTheme();
+        }}>
+        <View style={styles.themeToggleView}>
+          <View style={styles.textView}>
+            <Text>Dark Theme</Text>
+          </View>
+          <View pointerEvents="none">
+            <Switch role="switch" value={paperTheme.dark} />
+          </View>
+        </View>
+      </TouchableRipple>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  themeToggleView: {
+    flexDirection: 'row',
+    alignSelf: 'flex-end',
+    paddingVertical: '4%',
+    paddingHorizontal: '4%',
+  },
+  textView: {
+    marginTop: '0.7%',
+  },
+});
 
 export default SettingsScreen;
