8aeb533d49697e6fa9e1cee1b01257d4f750ccbf
Oreberry <aubreymonk1@gmail.com>
2021-05-12 14:10:22 +0100

    Add login and signup validation
    feat: login validation
    feat: signup validation

--- SmartHomeApp/src/screens/loginScreen.js
+++ SmartHomeApp/src/screens/loginScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View} from 'react-native';
+import {View, ToastAndroid} from 'react-native';
 import {Button, Text, TextInput} from 'react-native-paper';
 import Login from '../components/login';
 import globalStyle from '../styles/globalStyle';
@@ -12,11 +12,28 @@ const LoginScreen = (props) => {
   // const [password, setPassword] = useState('');
 
   const submit = () => {
-    const details = {
-      email,
-      password,
-    };
-    Login(props, details);
+    try {
+      const emailRegexString = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // to validate email
+      const passwordRegexString = /^\S{5,}$/; // more than 5 chars and no white space
+
+      // check if email is empty and if it matches the regex
+      if (email === '' || !emailRegexString.test(email)) {
+        ToastAndroid.show('Invalid email entered.', ToastAndroid.SHORT);
+        // check if password is empty or if it matches the regex
+      } else if (password === '' || !passwordRegexString.test(password)) {
+        ToastAndroid.show('Invalid password entered.', ToastAndroid.SHORT);
+      } else {
+        // create details object
+        const details = {
+          email,
+          password,
+        };
+        // call login function giving the details object
+        Login(props, details);
+      }
+    } catch (e) {
+      ToastAndroid.show('An Unexpected Error Has Occured', ToastAndroid.SHORT);
+    }
   };
 
   return (
