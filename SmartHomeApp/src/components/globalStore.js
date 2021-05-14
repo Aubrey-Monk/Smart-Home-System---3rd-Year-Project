9bd2b63157ff781f606f969651fb3ef5c20ad284
Oreberry <aubreymonk1@gmail.com>
2021-05-14 18:41:31 +0100

    Update globalStore.js

--- SmartHomeApp/src/components/globalStore.js
+++ SmartHomeApp/src/components/globalStore.js
@@ -1,3 +1,3 @@
-// for the server ip if using android emulator the ip should be 10.0.0.2, if using actual phone ip should be the ipv4 of the computer server is hosted on
-const globalStore = {serverIP: '192.168.0.15'};
+// for the server ip if using android emulator the ip should be 10.0.2.2, if using actual phone ip should be the ipv4 of the computer server is hosted on
+const globalStore = {serverIP: '10.0.2.2'};
 export default globalStore;
